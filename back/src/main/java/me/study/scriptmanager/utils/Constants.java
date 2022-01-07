aa00d60791ac17fdc3ba9acbc6cbd43f32cd5595
KostiantynKorzh <kostyantynkorzh@gmail.com>
2022-01-07 10:58:45 +0200

    React project setup. Separate back and front

--- back/src/main/java/me/study/scriptmanager/utils/Constants.java
+++ back/src/main/java/me/study/scriptmanager/utils/Constants.java
@@ -3,6 +3,5 @@ package me.study.scriptmanager.utils;
 public class Constants {
 
     public static final String BASH_PATH = "C:\\Program Files\\Git\\bin\\bash.exe";
-    public static final String BASE_DIR = "C:\\Users\\UX502785\\Desktop\\self-study\\script-manager\\src\\main\\java\\me\\study\\scriptmanager";
 
 }
---
+++ front/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = ()=> {
+  return (
+   <>
+   <h1>Just Testing...</h1>
+   </>
+  );
+}
+
+export default App;
